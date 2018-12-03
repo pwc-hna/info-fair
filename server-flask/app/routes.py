fbe0355e8d6832db66af0cfe81a017ba04510d24
Horia Naidin <horia-gabriel.naidin@pwc.com>
2018-12-03 11:47:59 +0100

    Working db

--- server-flask/app/routes.py
+++ server-flask/app/routes.py
@@ -1,36 +1,22 @@
 from app import app
 from flask import render_template, abort, request
 import json
+from db import *
 
-results = [
-        {
-            'username':'Horia',
-            'time':'00:12:99',
-            'mistakes':'1'
-        },
-        {
-            'username':'Robocop',
-            'time':'00:05:77',
-            'mistakes':'0'
-        }
-        ]
+db.create_all()
 
-live_players = [
-    {
-        'username' : 'sample_destroyer',
-        'current_doc' : '0',
-        'total_docs' : '20'
-    }
-]
+results = []
+
+live_players = []
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    global results
-    return render_template('index.html', title='Home', results=results,live_players=live_players)
+    return render_template('index.html', title='Home', results=db_get_all_finished_users_as_list(), live_players=db_get_all_live_users_as_list())
 
 
-@app.route('/finished', methods=['POST']) 
+@app.route('/finished', methods=['POST'])
 def finished():
     if not request.json:
         abort(400)
@@ -42,51 +28,38 @@ def finished():
         incoming_time = req_data['time']
         incoming_mistakes = req_data['mistakes']
         incoming_finishing_player = {
-                'username':incoming_username,
-                'time':incoming_time,
-                'mistakes':incoming_mistakes
-                }
+            'username': incoming_username,
+            'time': incoming_time,
+            'mistakes': incoming_mistakes
+        }
     except KeyError:
         print "ERROR invalid JSON data " + req_data
         return
 
-    # Populate our "db"
-    results.append(incoming_finishing_player)
-    for live_player in live_players:
-        if live_player['username'] == incoming_username:
-            live_players.remove(live_player)
-            break
+    db_add_finishing_player(incoming_finishing_player)
     return json.dumps(request.json)
 
 
-@app.route('/progress', methods=['POST']) 
+@app.route('/progress', methods=['POST'])
 def progress():
     if not request.json:
         print "ERROR no json"
         abort(400)
     req_data = request.json
 
-    # Validate input data
     try:
         incoming_username = req_data['username']
         incoming_current_document = req_data['current_doc']
         incoming_total_documents = req_data['total_docs']
         incoming_live_player = {
-                'username':incoming_username,
-                'current_doc':incoming_current_document,
-                'total_docs':incoming_total_documents
-                }
+            'username': incoming_username,
+            'current_doc': incoming_current_document,
+            'total_docs': incoming_total_documents
+        }
     except KeyError:
         print "ERROR invalid JSON data " + req_data
         return
 
-    found = False
-    for live_player in live_players:
-        if live_player['username'] == incoming_username:
-            found = True
-            # Replace the whole user entry
-            live_players[live_players.index(live_player)] = incoming_live_player
-            break
-    if not found:
-        live_players.append(incoming_live_player)
+    db_user_update_live_player(incoming_live_player)
+
     return json.dumps(request.json)
---
+++ server-flask/db.py
@@ -0,0 +1,90 @@
+from app import app
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+
+file_path = os.path.abspath(os.getcwd())+"\\test_database.db"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+file_path
+db = SQLAlchemy(app)
+
+class FinishedUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=False, nullable=False)
+    time = db.Column(db.String(120), unique=False, nullable=True)
+    mistakes = db.Column(db.String(120), unique=False, nullable=True)
+
+    def __repr__(self):
+        return '<FinishedUser %r %r %r>' % (self.username, self.time, self.mistakes)
+
+class LiveUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=False, nullable=False)
+    currentDoc = db.Column(db.String(120), unique=False, nullable=True)
+    totalDocs = db.Column(db.String(120), unique=False, nullable=True)
+
+    def __repr__(self):
+        return '<LiveUser %r %r %r>' % (self.username, self.currentDoc, self.totalDocs)
+
+
+def db_add_finished_user(username, time, mistakes):
+    user = FinishedUser(username=username, time=time, mistakes=mistakes)
+    db.session.add(user)
+    db.session.commit()
+    
+def db_add_live_user(username, currentDoc, totalDocs):
+    user = LiveUser(username=username, currentDoc=currentDoc, totalDocs=totalDocs)
+    db.session.add(user)
+    db.session.commit()
+
+
+def db_add_live_player(incoming_live_player):
+    db_add_live_user(username=incoming_live_player['username'],
+                     currentDoc=incoming_live_player['current_doc'], totalDocs=incoming_live_player['total_docs'])
+
+def db_add_finishing_player(incoming_finishing_player):
+    db_add_finished_user(username=incoming_finishing_player['username'], time=incoming_finishing_player['time'], 
+                     mistakes=incoming_finishing_player['mistakes'])
+    print "player finishing, remove it from the live db"
+    db_rm_live_player(incoming_finishing_player['username'])
+        
+def db_rm_live_player(username):
+    if (db_user_is_live(username)):
+        user = LiveUser.query.filter(LiveUser.username == username).first()
+        db.session.delete(user)
+        db.session.commit() 
+
+def db_user_is_live(username):
+    print "db_user_exists?"+ str(LiveUser.query.filter(LiveUser.username == username).first() is not None)
+    return LiveUser.query.filter(LiveUser.username == username).first() is not None
+
+def db_user_has_finished(username):
+    print "db_user_has_finished?"+ str(FinishedUser.query.filter(FinishedUser.username == username)).first() is not None
+    return FinishedUser.query.filter(FinishedUser.username == username).first() is not None
+
+def db_user_update_live_player(incoming_live_player):
+    username = incoming_live_player['username']
+    # Does it exist already?
+    if db_user_is_live(username):
+        user = LiveUser.query.filter(LiveUser.username == username).first()
+        print "User = "+ str(user)
+        user.currentDoc = incoming_live_player['current_doc']
+        user.totalDocs = incoming_live_player['total_docs']
+        db.session.commit()
+    else:
+        print "User doesn't exist, create it"
+        db_add_live_player(incoming_live_player)
+
+def db_get_all_live_users_as_list():
+    query_entries = LiveUser.query.all()
+    live_users = []
+    for live_user in query_entries:
+        live_users.append({'username':live_user.username,'current_doc':live_user.currentDoc, 'total_docs':live_user.totalDocs})
+    return live_users
+
+def db_get_all_finished_users_as_list():
+    query_entries = FinishedUser.query.all()
+    finished_users = []
+    for finished_player in query_entries:
+        finished_users.append({'username':finished_player.username,'time':finished_player.time, 'mistakes':finished_player.mistakes})
+    return finished_users
